d3138880ea00e3e329904c0af7d5f21fe4b2b717
piet <pprotassow@posteo.de>
2022-01-31 17:18:29 +0100

    replace with morgan

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const express = require("express")
-const path = require("path")
-const fs = require("fs")
+/** @format */
+
+const express = require('express')
+const morgan = require('morgan') //morgan is a function which return a middleware function 
+const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
+app.use(morgan('short'))
 app.use((req, res, next) => {
-    console.log("Request url:" + req.url)
-    console.log("Request date:" + new Date())
-    next()
-})
-
-app.use((req, res, next) => {
-    const filePath = path.join(__dirname, "static", req.url)
-    fs.stat(filePath, function(err, fileInfo) {
-        if(err) {
+    const filePath = path.join(__dirname, 'static', req.url)
+    fs.stat(filePath, function (err, fileInfo) {
+        if (err) {
             next() //we must call next since node is aynchronous; you need to tell express when to continue
-            return;
+            return
         }
-        if(fileInfo.isFile()) {
+        if (fileInfo.isFile()) {
             res.sendFile(filePath)
         } else {
             next()
@@ -28,10 +26,9 @@ app.use((req, res, next) => {
 //404 middleware; this must be the last middleware, since the next function is not called ==> order of the middleware stack is important
 app.use((req, res) => {
     res.status(404)
-    res.send("File not found")
+    res.send('File not found')
 })
 
 app.listen(3000, () => {
-    console.log("App starting on port 3000")
+    console.log('App starting on port 3000')
 })
-
