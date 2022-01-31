b0296380a29c38eb649f6ee0412a83bc46eb1e52
piet <pprotassow@posteo.de>
2022-02-01 00:04:04 +0100

    third party middleware

--- app.js
+++ app.js
@@ -3,25 +3,14 @@
 const express = require('express')
 const morgan = require('morgan') //morgan is a function which return a middleware function 
 const path = require('path')
-const fs = require('fs')
 
 const app = express()
 
 app.use(morgan('short'))
-app.use((req, res, next) => {
-    const filePath = path.join(__dirname, 'static', req.url)
-    fs.stat(filePath, function (err, fileInfo) {
-        if (err) {
-            next() //we must call next since node is aynchronous; you need to tell express when to continue
-            return
-        }
-        if (fileInfo.isFile()) {
-            res.sendFile(filePath)
-        } else {
-            next()
-        }
-    })
-})
+
+const staticPath = path.join(__dirname, "static")
+app.use(express.static(staticPath))
+/* app.use(express.static(staticPath, {index:'witcher.jpeg'})) */
 
 //404 middleware; this must be the last middleware, since the next function is not called ==> order of the middleware stack is important
 app.use((req, res) => {
