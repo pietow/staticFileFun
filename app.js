a4d92d2c1314feebe3aa91c333bec8b96dc654dc
piet <pprotassow@posteo.de>
2022-02-01 00:51:43 +0100

    parameters in route

--- app.js
+++ app.js
@@ -7,23 +7,16 @@ const path = require('path')
 const app = express()
 
 app.use(morgan('combined'))
-app.use((req, res, next) => {
-    console.log(req.method)
-    console.log(req.rawHeaders)
-    console.log(res.statusCode)
-    console.log(req.protocol)
-    next()
-})
 
-const staticPath = path.join(__dirname, "static")
-app.use(express.static(staticPath))
-/* app.use(express.static(staticPath, {index:'witcher.jpeg'})) */
+app.get('/users/:userid', (req, res) => {
+    const userId = Number(req.params.userid)
+    console.log(userId)
+    res.send('Welcome')
+})
 
 //404 middleware; this must be the last middleware, since the next function is not called ==> order of the middleware stack is important
 app.use((req, res) => {
-    console.log(req.method)
-    res.status(404)
-    res.send('File not found')
+    res.status(404).send('Page not found')
 })
 
 app.listen(3000, () => {
